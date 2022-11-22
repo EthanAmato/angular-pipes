f80037c74f277355b6304013229e55c3d633ff00
EthanAmato <ethannamato@gmail.com>
2022-11-22 09:56:07 -0500

    completed project on angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pipes';
+  amount = 223.4256;
+  channelName = "iLoveToFarm99";
+  score = 0.8945;
+  today = new Date();
+  name = "Ethan Amato";
+  names: string[] = ["Ethan Amato", "Elon Must", "Warren Buffet", "Selena Gomez", "Hillary Duff"]
+
+
 }
